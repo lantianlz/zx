574d36b12070ff96d36c27abd959a24f725ee7ab
lei286394973 <286394973@qq.com>
2013-12-29 02:40:32 -0800

    将question模板移动到question目录下

---
+++ www/question/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+
+urlpatterns = patterns('', 
+	url(r'^$', 'www.question.views.question_home'),
+	url(r'^question_detail/?$', 'www.question.views.question_detail'),
+)
