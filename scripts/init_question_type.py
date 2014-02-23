118669a1a9ae5667de7090d62e3024b287635207
simplejoy <lantian-lz@163.com>
2014-02-23 21:12:48 +0800

    answer isself check

--- scripts/init_question_type.py
+++ scripts/init_question_type.py
@@ -14,8 +14,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'www.settings'
 
 def init_question_type():
     from www.question.models import QuestionType
-    QuestionType.objects.all().delete()
-    datas = [(1, u'大盘', 'dapan'), (2, u'个股', 'gegu'), (3, u'债券', 'zhaiquan'), (4, u'期权', 'qiquan')]
+    # QuestionType.objects.all().delete()
+    datas = [(1, u'大盘', 'dapan'), (2, u'个股', 'gegu'), (3, u'其他', 'qita'),]
     for data in datas:
         QuestionType.objects.create(name=data[1], value=data[0], domain=data[2])
     print 'ok'
