ee731d1a816b2b469fea1ee3011b749475a98e16
lei286394973 <286394973@qq.com>
2014-03-05 06:36:40 -0800

    IE下宽度bug

--- static/js/site/user_profile.js
+++ static/js/site/user_profile.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	// 左侧切换事件
 	$('.stats a').bind('click', function(){
 		var target = $(this).attr('href');
-		alert(target)
-		//$('.user-profile-main a[href='+$(this).attr('href')+']').tab('show');
-		//$('html,body').animate({scrollTop: 0});
+		if(target.indexOf('#') > -1){
+			$('.user-profile-main a[href='+$(this).attr('href')+']').tab('show');
+			$('html,body').animate({scrollTop: 0});
+		}
+		
 	});
 
 	$('.avatar-100')
