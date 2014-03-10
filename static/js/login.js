5dc40a9b49dc7f50daa3690edc4a7c514042130a
lei286394973 <286394973@qq.com>
2014-03-10 07:08:03 -0700

    logo换上

--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // 鼠标移动到第三方登录时的事件
     $('.img-hover').bind('mouseenter', function(){
         var me = $(this), type = me.data('type');
         me.animate({'opacity': '0.3'}, 200, function(){
@@ -14,6 +15,7 @@ $(document).ready(function(){
         });
     });
 
+    // 登记按钮页面跳转
     $('.regist').bind('click', function(){
         window.location.href = '/regist';
     });
