4bcc08d599befbc0ca003c0643570a0b13adf48c
lei286394973 <286394973@qq.com>
2014-03-10 03:45:02 -0700

    修改登录界面样式

---
+++ static/js/login.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.img-hover').bind('mouseenter', function(){
+        var me = $(this), type = me.data('type');
+        me.animate({'opacity': '0.3'}, 200, function(){
+            me.attr('src', '/static/img/common/'+type+'-active.png');
+            me.animate({'opacity': '0.99'}, 200);
+        });
+    })
+    .bind('mouseleave', function(){
+        var me = $(this), type = me.data('type');
+        me.animate({'opacity': '0.3'}, 200, function(){
+            me.attr('src', '/static/img/common/'+type+'.png');
+            me.animate({'opacity': '0.99'}, 200);
+        });
+    });
+
+    $('.regist').bind('click', function(){
+        window.location.href = '/regist';
+    });
+});
--- www/account/views.py
+++ www/account/views.py
@@ -22,7 +22,7 @@ def show_index(request):
         return HttpResponseRedirect('/login')
 
 
-def login(request, template_name='account/login.html'):
+def login(request, template_name='account/login_bg.html'):
     email = request.POST.get('email', '').strip()
     password = request.POST.get('password', '').strip()
 
