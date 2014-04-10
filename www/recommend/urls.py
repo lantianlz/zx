75d84b81ce1b2754f3c8c7665b5300e5288ce4b8
simplejoy <lantian-lz@163.com>
2014-04-10 17:52:18 +0800

    rm invalid file

--- www/recommend/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-
-urlpatterns = patterns('', 
-	url(r'^$', 'www.recommend.views.recommend_home'),
-	url(r'^recommend_detail/?$', 'www.recommend.views.recommend_detail'),
-)
