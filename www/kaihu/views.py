2e708edc412f8e6d9efb8a1c5f10fe0deeb2fdeb
lei286394973 <286394973@qq.com>
2014-05-17 16:16:22 +0800

    添加开户详情页面

--- www/kaihu/views.py
+++ www/kaihu/views.py
@@ -6,11 +6,17 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-def home(request, template_name='kaihu/department_list.html'):
-    
+
+def home(request, template_name='kaihu/home.html'):
+
+    return render_to_response(template_name, locals(), context_instance=RequestContext(request))
+
+
+def department_list(request, area_id, template_name='kaihu/department_list.html'):
+
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
 
 def department_detail(request, department_id, template_name='kaihu/department_detail.html'):
-    
+
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
