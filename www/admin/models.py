49d10578f635a97190f9d2c5f862366370a63ef0
lei286394973 <286394973@qq.com>
2014-05-21 16:50:20 +0800

    索引

--- www/admin/models.py
+++ www/admin/models.py
@@ -8,7 +8,7 @@ class Permission(models.Model):
     '''
     权限类 
     '''
-    name = models.CharField(verbose_name=u'权限名称', max_length=64)
+    name = models.CharField(verbose_name=u'权限名称', max_length=64, unique=True)
     code = models.CharField(verbose_name=u'权限代码', max_length=32, unique=True)
     parent = models.ForeignKey('self', verbose_name=u'父权限', related_name="children", null=True)
 
@@ -21,9 +21,9 @@ class UserPermission(models.Model):
     '''
     用户权限表
     '''
-    user_id = models.CharField(verbose_name=u'用户', max_length=32)
+    user_id = models.CharField(verbose_name=u'用户', max_length=32, db_index=True)
     permission = models.ForeignKey(Permission, verbose_name=u'权限')
-    create_time = models.DateTimeField(verbose_name=u'创建时间', db_index=True, auto_now_add=True)
+    create_time = models.DateTimeField(verbose_name=u'创建时间', auto_now_add=True)
     creator = models.CharField(verbose_name=u'创建者', max_length=32)
 
     class Meta:
