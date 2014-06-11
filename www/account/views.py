2652b052aca3acac32cc7c6046c734f1fef667be
simplejoy <lantian-lz@163.com>
2014-06-11 14:33:23 +0800

    未登录首页由登陆页修改为问答首页

--- www/account/views.py
+++ www/account/views.py
@@ -25,7 +25,8 @@ def show_index(request):
         from www.timeline.views import show_user_timeline
         return show_user_timeline(request)
     else:
-        return login(request)
+        from www.question.views import question_home
+        return question_home(request)
 
 
 def login(request, template_name='account/login_bg.html'):
