21b37cf70ef9bb9052adeaa6a5127f0473cb0fc5
simplejoy <lantian-lz@163.com>
2014-07-01 19:07:00 +0800

    修改专题模型

--- www/zhuanti/models.py
+++ www/zhuanti/models.py
@@ -5,7 +5,12 @@ from django.db import models
 
 class ZhuanTi(models.Model):
 
-    state_choices = ((0, u'无效用户'), (1, u'有效用户'), (2, u'内部成员'), )
+    title = models.CharField(max_length=64, unique=True)
+    summary = models.TextField()
+    img = models.CharField(max_length=128)
+    author_name = models.CharField(max_length=128)  # 作者姓名
+    domain = models.CharField(max_length=16, unique=True)  # 模板名称同样使用这个字段
+    # tempalte_name = models.CharField(max_length=64, unique=True)
 
     state = models.BooleanField(default=True, db_index=True)
     create_time = models.DateTimeField(auto_now_add=True)
--- www/zhuanti/views.py
+++ www/zhuanti/views.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import json
-# import urllib
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from common import utils, page
-from www.misc.decorators import member_required
+# from www.misc.decorators import member_required
 
 
 def index(request, template_name='zhuanti/zhuanti_list.html'):
