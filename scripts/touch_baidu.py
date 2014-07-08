636e7bb6be46799efb88f3b8ea7cd4a4ca7bdecf
simplejoy <lantian-lz@163.com>
2014-07-08 16:41:54 +0800

    完善排名抓取

--- scripts/touch_baidu.py
+++ scripts/touch_baidu.py
@@ -4,7 +4,7 @@ import requests
 
 
 def touch_baidu(key):
-    for page in range(20000, 30000, 10):
+    for page in range(0, 1000, 10):
         print page
         url = u"http://www.baidu.com/s?wd=" + key + "&pn=" + \
             str(page) + u"&oq=" + key + "&tn=63090008_1_hao_pg&ie=utf-8&usm=2"
@@ -16,5 +16,5 @@ def touch_baidu(key):
     print rep.text.encode('utf8')
 
 if __name__ == '__main__':
-    key = u"成都股票开户"
+    key = u"重庆股票开户"
     touch_baidu(key)
