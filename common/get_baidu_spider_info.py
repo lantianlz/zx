f48529d6f2bd408fa019287d69053af1516125c4
simplejoy <lantian-lz@163.com>
2014-07-25 17:09:25 +0800

    返回值完善

--- common/get_baidu_spider_info.py
+++ common/get_baidu_spider_info.py
@@ -22,7 +22,7 @@ from pprint import pprint
 def get_baidu_spider_info():
     cmd_baidu = """grep -i Baiduspider /var/log/nginx/www.log"""
     datas = commands.getoutput(cmd_baidu)
-    lst_info = []
+    lst_all_info = []
     lst_invalid_info = []
     dict_info_group_by_url = {}
 
@@ -32,20 +32,21 @@ def get_baidu_spider_info():
             # print data
             state = data[8]
             access_time = datetime.datetime.strptime(data[3].split()[0][1:], '%d/%b/%Y:%H:%M:%S')
-            lst_info.append([data[0], access_time.strftime("%Y-%m-%d %H:%M:%S"), data[6], state])
+            lst_all_info.append([data[0], access_time.strftime("%Y-%m-%d %H:%M:%S"), data[6], state])
             if state != "200":
                 lst_invalid_info.append([data[0], access_time.strftime("%Y-%m-%d %H:%M:%S"), data[6], state])
 
-    for info in lst_info:
+    for info in lst_all_info:
         count = dict_info_group_by_url.get(info[2], 0)
         dict_info_group_by_url[info[2]] = count + 1
 
     lst_info_group_by_url = dict_info_group_by_url.items()
     lst_info_group_by_url.sort(key=lambda x: x[1], reverse=True)
 
-    pprint(lst_info)
+    # pprint(lst_all_info)
     # pprint(lst_info_group_by_url)
     # pprint(lst_invalid_info)
+    return lst_all_info, lst_invalid_info, lst_info_group_by_url
 
 
 if __name__ == '__main__':
