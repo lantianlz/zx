a42b8a7831c167d66186a1a9c7c1e29c54f5fcf8
simplejoy <lantian-lz@163.com>
2014-07-31 16:56:50 +0800

    1.百度绿色通道url后加html后缀 2.settings默认时间格式完善

--- www/urls.py
+++ www/urls.py
@@ -40,6 +40,7 @@ urlpatterns = patterns('',
                        url(r'^stock/', include('www.stock.urls')),
 
                        url(r'^baidu_map$', 'www.misc.views.baidu_map'),
+                       url(r'^baidu_map.html$', 'www.misc.views.baidu_map'),
                        url(r'^sitemap.xml$', 'www.misc.views.sitemap'),
                        # url(r'^8efc199540223825b0fb2026cc9b8e39\.html$', 'www.misc.views.link_view'),
                        url(r'^(?P<txt_file_name>\w+)\.txt$', 'www.misc.views.txt_view'),
