a7ff62ce0f8637aacabd5993274b2deaad9615d7
lei286394973 <286394973@qq.com>
2014-08-21 15:54:14 +0800

    后台修改营业部所属城市

--- www/admin/views_department.py
+++ www/admin/views_department.py
@@ -120,11 +120,11 @@ def modify_department(request):
     name = request.REQUEST.get('name')
     sort_num = request.REQUEST.get('sort')
     tel = request.REQUEST.get('tel')
-    # city_id = request.REQUEST.get('belong_city')
+    city_id = request.REQUEST.get('belong_city')
     # company_id = request.REQUEST.get('belong_company')
     addr = request.REQUEST.get('addr')
     des = request.REQUEST.get('des')
 
     return DepartmentBase().modify_department(
-        department_id, name=name, sort_num=sort_num, tel=tel, addr=addr, des=des
+        department_id, name=name, sort_num=sort_num, tel=tel, addr=addr, des=des, city_id=city_id
     )
