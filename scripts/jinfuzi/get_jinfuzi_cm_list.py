258e2008b004d9750d3d8428c23281e438cb9258
simplejoy <lantian-lz@163.com>
2014-09-24 11:55:29 +0800

    脚本完善

--- scripts/jinfuzi/get_jinfuzi_cm_list.py
+++ scripts/jinfuzi/get_jinfuzi_cm_list.py
@@ -30,18 +30,17 @@ def get_jinfuzi_cm_list():
     citys = city_list(".md_uc_box dd>a")
 
     print datetime.datetime.now()
+    continue_flag = True
     # 获取城市
     for city in citys:
         city = pq(city)
         city_url_first = city.attr("href")
         city_name = city.html()
 
-        if city_name in [u"北京", u"上海", u"天津", u"重庆", u"茂名", u"云浮", u"清远", u"揭阳", u"中山", u"潮州", u"湛江", u"深圳", u"汕尾", u"惠州", u"梅州", u"珠海", u"东莞", u"肇庆", u"江门", u"佛山", u"汕头",
-                         u"韶关", u"河源", u"广州", u"南通", u"启东", u"宿迁", u"南京", u"无锡", u"苏州", u"连云港", u"徐州", u"镇江", u"淮安", u"盐城", u"泰州", u"扬州", u"常州", u"济南", u"莱芜", u"青岛", u"潍坊", u"淄博",
-                         u"东营", u"日照", u"泰安", u"菏泽", u"临沂", u"济宁", u"聊城", u"枣庄", u"德州", u"杭州", u"瑞安", u"温州", u"舟山", u"台州", u"嘉兴", u"衢州", u"绍兴", u"宁波", u"金华", u"郑州", u"周口", u"新乡",
-                         u"洛阳", u"灵宝", u"孟州", u"漯河", u"驻马店", u"三门峡", u"开封", u"安阳", u"鹤壁", u"平顶山", u"商丘", u"濮阳", u"南阳", u"许昌", u"信阳", u"石家庄", u"承德", u"秦皇岛", u"保定", u"衡水", u"沧州",
-                         u"邢台", u"邯郸", u"廊坊", u"唐山", u"沈阳", u"辽阳", u"营口", u"抚顺", u"大连", u"盘锦", u"成都", u"达州", u"宜宾", u"简阳市", u"南充", u"泸州", u"内江", u"巴中", u"广元"]:
-            continue
+        # if city_name != u"岳阳" and continue_flag:
+        #     continue
+        # else:
+        #     continue_flag = False
 
         # 获取营业部
         for i in range(1, 50):
@@ -62,9 +61,15 @@ def get_jinfuzi_cm_list():
                 department_name = department.html()
 
                 # 获取客户经理
-                resp = requests.get(department_url, headers=headers, timeout=30)
-                resp.encoding = "utf8"
-                cm_list = pq(resp.text)
+                for i in range(3):
+                    try:
+                        resp1 = requests.get(department_url, headers=headers, timeout=10)
+                        break
+                    except:
+                        print u"timeout in line 66"
+
+                resp1.encoding = "utf8"
+                cm_list = pq(resp1.text)
                 cms_vip = cm_list(".yyby-content-list-vip h3 a:odd")
                 cms_normal = cm_list(".yyby-content-list h3 a:odd")
 
