5c114d3fbb84628f8b9a6b83adc5fc1aa525aefc
simplejoy <lantian-lz@163.com>
2014-11-21 15:37:48 +0800

    copyright信息自动迭代

--- www/misc/context_processors.py
+++ www/misc/context_processors.py
@@ -11,6 +11,7 @@ def config(request):
     """
     @attention: Adds settings-related context variables to the context.
     """
+    import datetime
     from django.conf import settings
     from common import cache
 
@@ -20,5 +21,6 @@ def config(request):
         'MEDIA_VERSION': cache.Cache(cache.CACHE_STATIC).get('media_version') or '000',  # 从缓存中取版本号
         'SERVER_DOMAIN': settings.SERVER_DOMAIN,
         'MAIN_DOMAIN': settings.MAIN_DOMAIN,
-        'IMG0_DOMAIN': settings.IMG0_DOMAIN
+        'IMG0_DOMAIN': settings.IMG0_DOMAIN,
+        "YEAR": datetime.datetime.now().strftime("%Y"),
     }
