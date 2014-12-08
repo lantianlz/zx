411b08d4f39d171ae223adbce3d9af6cbccb7b29
lei286394973 <286394973@qq.com>
2014-12-08 16:01:39 +0800

    成都广告

--- www/custom_tags/templatetags/custom_tags.py
+++ www/custom_tags/templatetags/custom_tags.py
@@ -202,7 +202,11 @@ def kaihu_ad(context):
     from common import utils
 
     city_abbr = utils.get_sub_domain_from_http_host(context['request'].META.get('HTTP_HOST', ''))
-    dict_ads = {"mz": ["ad_mz.jpg", "8655809", u"梅州"], "my": ["ad_my.jpg", "1919994989", u"绵阳"],}
+    dict_ads = {
+        "mz": ["ad_mz.jpg", "8655809", u"梅州"], 
+        "my": ["ad_my.jpg", "1919994989", u"绵阳"], 
+        "chengdu": ["ad_chengdu.jpg", "2277483400", u"成都"],
+    }
     default = ["ad_common.jpg", "403897485", u"通用"]
     ad_img = dict_ads.get(city_abbr, default)
 
