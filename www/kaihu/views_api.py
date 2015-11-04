fbed2a7a1c2fa14567ab49eb0f58a441a6bbbacc
stranger <286394973@qq.com>
2015-11-04 20:57:51 +0800

    app

--- www/kaihu/views_api.py
+++ www/kaihu/views_api.py
@@ -139,7 +139,6 @@ def api_get_custom_manager_list(request):
     if city.id == 3:
         custom_managers += cmb.format_customer_managers_for_ajax([
             cmb.get_customer_manager_by_user_id(user_id="fd3646d808d111e581ea00163e003240"), 
-            cmb.get_customer_manager_by_user_id(user_id="a92af6808bd911e48d6a00163e003240"), 
         ])
 
     # =============== js 获取省份下面所有城市id ===============
@@ -163,7 +162,6 @@ def api_get_custom_manager_list(request):
     # 整个湖南单独处理
     if city.id in [1794, 1804, 1814, 1820, 1833, 1846, 1856, 1866, 1871, 1878, 1890, 1902, 1915, 1921]:
         custom_managers = custom_managers + cmb.format_customer_managers_for_ajax([
-            cmb.get_customer_manager_by_user_id(user_id="b1f4299c296511e5b2b800163e003240"), 
             cmb.get_customer_manager_by_user_id(user_id="f7957d0656b111e5aad300163e003240"), 
         ])
 
--- www/toutiao/test_toutiao.py
+++ www/toutiao/test_toutiao.py
@@ -262,196 +262,193 @@ class WeixinSpider(object):
     def sync(self):
         mps = self.mps
 
-        for mp in mps:
+        mp = mps[0]
+        proxy = '177.157.82.209:8080'
+        
+        for proxy in self.proxies:
             
+            resp = requests.get(
+                u'http://weixin.sogou.com/weixin?type=1&query=%s&ie=utf8' % mp['name'],
+                headers = self.HEADERS,
+                proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy},
+                timeout = 15
+            )
+            print '-------'
+            cookies = resp.cookies
+            temp = pq(resp.text)
+            href = temp('.wx-rb_v1').eq(0).attr('href')
+            ext = re.compile('ext=(.*)').search(href).groups()[0]
+
+            print u'http://weixin.sogou.com/weixin?type=1&query=%s&ie=utf8' % mp['name']
+            print href
+            print ext
+            print '1-1--1-1-1-1-1-1-1-1-1-1-1-1-1-1-1--1-1-1-1-1'
+
+            # url = u"http://weixin.sogou.com/gzh?openid=%s&ext=%s" % (mp['open_id'], ext)
+            # resp = requests.get(
+            #     url,
+            #     headers = self.HEADERS,
+            #     # proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy},
+            #     timeout = 15,
+            #     cookies = cookies
+            # )
+            # cookies = resp.cookies
+
+            url = u"http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=%s&ext=%s&gzhArtKeyWord=&%s&page=1" % (mp['open_id'], ext, mp['ext_id'])
+            resp = requests.get(
+                url,
+                headers = self.HEADERS,
+                proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy},
+                timeout = 15,
+                cookies = cookies
+            )
+            print url
+            print '2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2'
+            lst_article = eval(re.compile('gzhcb\((.+)\)').findall(resp.text)[0])["items"]
+            # cookies = resp.cookies
+
+            article = lst_article[0]
+            article = article.replace("\\", "")
+            url = re.compile('<url>(.+)</url>').findall(article)[0][9:-3]
+            url = u"http://weixin.sogou.com" + url
+            print url
+            resp = requests.get(
+                url,
+                headers = self.HEADERS,
+                timeout = 15,
+                cookies = cookies,
+                proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy}
+            )
+
+            print resp.text
+            print '3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3'
+
+
+def get_mps():
+    '''
+    获取微信公众号数据
+    '''
+    url = "http://%s/toutiao/get_mps" % "www.zhixuan.com"
+    req = requests.get(url)
+    return json.loads(req.text)
+
+def sync2():
+    UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
+
+    mps = get_mps()
+    # proxies = get_proxies()
+
+    dcap = dict(DesiredCapabilities.PHANTOMJS)
+    dcap["phantomjs.page.settings.userAgent"] = (
+        UA
+    )
+
+    for mp in mps:
+        try:
+
+            # driver = webdriver.PhantomJS(desired_capabilities=dcap, executable_path='/Users/stranger/workspace/phantomjs/bin/phantomjs')
+            driver = webdriver.Firefox()
+            driver.set_window_size(1120, 550)
+
+            time.sleep(6)
+            # 1.访问微信
+            url = u"http://weixin.sogou.com/weixin?type=1&query=a"
+            driver.get(url)
+            print url
             
-            for proxy in self.proxies:
-
-                try:
-                    resp = requests.get(
-                        u'http://weixin.sogou.com/weixin?type=1&query=%s&ie=utf8' % mp['name'],
-                        headers = self.HEADERS,
-                        proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy},
-                        timeout = 15
-                    )
-                    print '-------'
-                    cookies = resp.cookies
-                    temp = pq(resp.text)
-                    href = temp('.wx-rb_v1').eq(0).attr('href')
-                    ext = re.compile('ext=(.*)').search(href).groups()[0]
-
-                    print u'http://weixin.sogou.com/weixin?type=1&query=%s&ie=utf8' % mp['name']
-                    print href
-                    print ext
-                    print '1-1--1-1-1-1-1-1-1-1-1-1-1-1-1-1-1--1-1-1-1-1'
-
-                    # url = u"http://weixin.sogou.com/gzh?openid=%s&ext=%s" % (mp['open_id'], ext)
-                    # resp = requests.get(
-                    #     url,
-                    #     headers = self.HEADERS,
-                    #     # proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy},
-                    #     timeout = 15,
-                    #     cookies = cookies
-                    # )
-                    # cookies = resp.cookies
-
-                    url = u"http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=%s&ext=%s&gzhArtKeyWord=&%s&page=1" % (mp['open_id'], ext, mp['ext_id'])
-                    resp = requests.get(
-                        url,
-                        headers = self.HEADERS,
-                        proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy},
-                        timeout = 15,
-                        cookies = cookies
-                    )
-                    print url
-                    print '2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2'
-                    lst_article = eval(re.compile('gzhcb\((.+)\)').findall(resp.text)[0])["items"]
-                    # cookies = resp.cookies
-
-                    article = lst_article[0]
-                    article = article.replace("\\", "")
-                    url = re.compile('<url>(.+)</url>').findall(article)[0][9:-3]
-                    url = u"http://weixin.sogou.com" + url
-                    print url
-                    resp = requests.get(
-                        url,
-                        headers = self.HEADERS,
-                        timeout = 15,
-                        cookies = cookies,
-                        proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy}
-                    )
-
-                    print resp.text
-                    print '3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3'
-
-                    article_detail = pq(resp.text)
-                    #print url
-                    title = article_detail("#activity-name").html().split("<em")[0].strip()
-                    content = article_detail("#js_content").html()
-                    format_content = _replace_html_tag(content).strip()
-                    if len(format_content) < 300:
-                        continue
-
-                except Exception, e:
-                    print traceback.print_exc()
-                    continue 
-
-# def sync2():
-#     UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
-
-#     mps = get_mps()
-#     proxies = get_proxies()
-
-#     dcap = dict(DesiredCapabilities.PHANTOMJS)
-#     dcap["phantomjs.page.settings.userAgent"] = (
-#         UA
-#     )
-
-#     for mp in mps:
-#         try:
-
-#             driver = webdriver.PhantomJS(desired_capabilities=dcap, executable_path='/Users/stranger/workspace/phantomjs/bin/phantomjs')
-#             driver.set_window_size(1120, 550)
-
-#             time.sleep(6)
-#             # 1.访问微信
-#             url = u"http://weixin.sogou.com/weixin?type=1&query=a"
-#             driver.get(url)
-#             print url
+            time.sleep(6)
+            print driver.page_source
+            search_input = driver.find_elements_by_id('upquery')[0]
+            search_input.send_keys(Keys.BACKSPACE)
+            time.sleep(0.2)
+            search_input.send_keys(Keys.BACKSPACE)
+            time.sleep(0.2)
+            for x in range(len(mp['name'])):
+                search_input.send_keys(mp['name'][x])
+                time.sleep(0.2)
             
-#             time.sleep(6)
-#             print driver.page_source
-#             search_input = driver.find_elements_by_id('upquery')[0]
-#             search_input.send_keys(Keys.BACKSPACE)
-#             time.sleep(0.2)
-#             search_input.send_keys(Keys.BACKSPACE)
-#             time.sleep(0.2)
-#             for x in range(len(mp['name'])):
-#                 search_input.send_keys(mp['name'][x])
-#                 time.sleep(0.2)
-            
-#             print search_input.get_attribute('value')
+            print search_input.get_attribute('value')
 
-#             # 2.搜索公众号 得到ext
-#             driver.find_elements_by_id('searchBtn')[0].click()
-#             time.sleep(6)
-#             print driver.title 
-#             href = driver.find_elements_by_class_name('wx-rb_v1')[0].get_attribute('href')
-#             ext = re.compile('ext=(.*)').search(href).groups()[0]
-#             print ext
+            # 2.搜索公众号 得到ext
+            driver.find_elements_by_id('searchBtn')[0].click()
+            time.sleep(6)
+            print driver.title 
+            href = driver.find_elements_by_class_name('wx-rb_v1')[0].get_attribute('href')
+            ext = re.compile('ext=(.*)').search(href).groups()[0]
+            print ext
 
-#             url = u"http://weixin.sogou.com/gzh?openid=%s&ext=%s" % (mp['open_id'], ext)
-#             print url
+            url = u"http://weixin.sogou.com/gzh?openid=%s&ext=%s" % (mp['open_id'], ext)
+            print url
 
-#             # 3.搜索具体公众号
-#             driver.get(url)
-#             time.sleep(6)
+            # 3.搜索具体公众号
+            driver.get(url)
+            time.sleep(6)
 
-#             articles = driver.find_elements_by_class_name('news_lst_tab')
-#             dates = driver.find_elements_by_class_name('s-p')
-#             imgs = driver.find_elements_by_class_name('img_box2')
-#             temps = []
-#             for i in range(len(articles)):
-#                 href = articles[i].get_attribute('href')
-#                 create_time = dates[i].get_attribute('t')
-#                 img = imgs[i].find_element_by_tag_name('img').get_attribute('src')
-#                 temp = img.split('&url=')
-#                 temp.insert(1, '&w=210&h=105&url=')
-#                 img = "".join(temp)
+            articles = driver.find_elements_by_class_name('news_lst_tab')
+            dates = driver.find_elements_by_class_name('s-p')
+            imgs = driver.find_elements_by_class_name('img_box2')
+            temps = []
+            for i in range(len(articles)):
+                href = articles[i].get_attribute('href')
+                create_time = dates[i].get_attribute('t')
+                img = imgs[i].find_element_by_tag_name('img').get_attribute('src')
+                temp = img.split('&url=')
+                temp.insert(1, '&w=210&h=105&url=')
+                img = "".join(temp)
 
-#                 temps.append([href, datetime.datetime.fromtimestamp(float(create_time)), img])
+                temps.append([href, datetime.datetime.fromtimestamp(float(create_time)), img])
 
-#             for temp in temps:
-#                 href = temp[0]
-#                 create_time = temp[1]
-#                 print href, create_time
+            for temp in temps:
+                href = temp[0]
+                create_time = temp[1]
+                print href, create_time
 
-#                 # 4.跳转到微信
-#                 driver.get(href)
-#                 time.sleep(6)
-#                 title = driver.find_elements_by_id('activity-name')[0].text
-#                 content = driver.find_elements_by_id('js_content')[0].get_attribute('innerHTML')
-#                 url = driver.current_url
-#                 img = temp[2]
-#                 # try:
-#                 #     img = img_prefix + driver.find_elements_by_id('js_cover')[0].get_attribute('src')
-#                 # except Exception, e:
-#                 #     print traceback.print_exc()
-#                 #     continue
+                # 4.跳转到微信
+                driver.get(href)
+                time.sleep(6)
+                title = driver.find_elements_by_id('activity-name')[0].text
+                content = driver.find_elements_by_id('js_content')[0].get_attribute('innerHTML')
+                url = driver.current_url
+                img = temp[2]
+                # try:
+                #     img = img_prefix + driver.find_elements_by_id('js_cover')[0].get_attribute('src')
+                # except Exception, e:
+                #     print traceback.print_exc()
+                #     continue
 
-#                 print title
-#                 print url
-#                 print img
+                print title
+                print url
+                print img
 
-#                 response = requests.post(
-#                     "http://%s/toutiao/sync_toutiao" % host,
-#                     data = {
-#                         'mp_id': mp['mp_id'],
-#                         'title': title,
-#                         'content': content,
-#                         'url': url,
-#                         'img': img,
-#                         'create_time': str(create_time)
-#                     }
-#                 )
+                response = requests.post(
+                    "http://%s/toutiao/sync_toutiao" % host,
+                    data = {
+                        'mp_id': mp['mp_id'],
+                        'title': title,
+                        'content': content,
+                        'url': url,
+                        'img': img,
+                        'create_time': str(create_time)
+                    }
+                )
 
-#                 result = json.loads(response.text)
+                result = json.loads(response.text)
 
-#                 if result['code'] == 0:
-#                     print u'成功!'
-#                 else:
-#                     print u'失败！！[%s]' % result['code']
+                if result['code'] == 0:
+                    print u'成功!'
+                else:
+                    print u'失败！！[%s]' % result['code']
 
-#                     if result['code'] == 2:
-#                         print u'此公众号暂无更新，跳过...'
-#                         time.sleep(80)
-#                         break
+                    if result['code'] == 2:
+                        print u'此公众号暂无更新，跳过...'
+                        time.sleep(80)
+                        break
 
-#         except Exception, e:
-#             print traceback.print_exc()
-#             continue
+        except Exception, e:
+            print traceback.print_exc()
+            continue
 
-#         driver.quit()
+        driver.quit()
 
 
     
@@ -461,6 +458,8 @@ if  __name__=='__main__':
     #     time.sleep(10)
     #     sync2()
 
-    WeixinSpider().sync()
+    # WeixinSpider().sync()
+
+    sync2()
 
 
